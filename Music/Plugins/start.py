a7a455191ae568128064059dcb0ab226f749a8a0
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-11-29 18:58:48 +0530

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -68,7 +68,7 @@ async def welcome(_, message: Message):
         except:
             return
 
-@Client.on_message(filters.group & filters.command(["start", "help"]))
+@Client.on_message(filters.group & filters.command(["startop", "helpop"]))
 async def start(_, message: Message):
     chat_id = message.chat.id
 #    if not await is_served_chat(chat_id):
